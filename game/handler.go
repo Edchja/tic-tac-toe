ad14b508258b0edc1bf005566e4596b5672255a8
Edgar Timakin <edgar.timakin@aracom.de>
2021-08-17 16:56:10 +0200

    refactored game handler and main

--- game/handler.go
+++ game/handler.go
@@ -1,8 +1,84 @@
 package game
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"edchja.de/tic-tac-toe/fonts"
+)
+
 var (
 	XWon, OWon, Tie bool
+	reader          = bufio.NewReader(os.Stdin)
 )
+
+func SetInput() int {
+	fmt.Println("\u001b[37mEnter a number: \u001b[0m")
+
+	input, _ := reader.ReadString('\n')
+	input = strings.Replace(input, "\r\n", "", -1)
+
+	inputInt, _ := strconv.Atoi(input)
+
+	return inputInt
+}
+
+func ProcessInput(gameBoardArr [][]int, input, player int) {
+	if input >= 1 && input <= 9 {
+		switch input {
+		case 1:
+			ProcessMove(gameBoardArr, 2, 0, player)
+		case 2:
+			ProcessMove(gameBoardArr, 2, 1, player)
+		case 3:
+			ProcessMove(gameBoardArr, 2, 2, player)
+		case 4:
+			ProcessMove(gameBoardArr, 1, 0, player)
+		case 5:
+			ProcessMove(gameBoardArr, 1, 1, player)
+		case 6:
+			ProcessMove(gameBoardArr, 1, 2, player)
+		case 7:
+			ProcessMove(gameBoardArr, 0, 0, player)
+		case 8:
+			ProcessMove(gameBoardArr, 0, 1, player)
+		case 9:
+			ProcessMove(gameBoardArr, 0, 2, player)
+		}
+	} else if player == 1 {
+		fonts.PrintErrorMessage("wrong input!")
+
+		fonts.PrintBoard(gameBoardArr)
+
+		playerInput := SetInput()
+		ProcessInput(gameBoardArr, playerInput, player)
+	} else if player == 2 {
+		// computer := getRandomNumber(1, 9)
+		// processInput(computer, player)
+	}
+}
+
+func ProcessMove(gameBoardArr [][]int, row, col, player int) {
+	switch {
+	case gameBoardArr[row][col] == 0:
+		gameBoardArr[row][col] = player
+
+	case gameBoardArr[row][col] != 0 && player != 2:
+		fonts.PrintErrorMessage("field already set!")
+
+		fonts.PrintBoard(gameBoardArr)
+
+		playerInput := SetInput()
+		ProcessInput(gameBoardArr, playerInput, player)
+
+	case gameBoardArr[row][col] != 0 && player != 1 && Condition(gameBoardArr, player) != true:
+		computer := GetRandomNumber(1, 9)
+		ProcessInput(gameBoardArr, computer, player)
+	}
+}
 
 func Condition(gameBoardArr [][]int, player int) bool {
 	var (
@@ -39,4 +115,15 @@ func Condition(gameBoardArr [][]int, player int) bool {
 	}
 
 	return false
+}
+
+func PrintWinningAnimation() {
+	switch {
+	case XWon:
+		fonts.PrintFont(fonts.XWonFont, 45)
+	case OWon:
+		fonts.PrintFont(fonts.OWonFont, 45)
+	case Tie:
+		fonts.PrintFont(fonts.TieFont, 25)
+	}
 }
